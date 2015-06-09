d3e7067640aa4113b2825f19c868f86dfd73faac
kzerbe <vmadmin@localhost.localdomain>
2015-06-09 16:59:46 +0200

    added FlowMod converter classes

---
+++ master/include/lib/eos/OpenFlowActionConverter.h
@@ -0,0 +1,18 @@
+#ifndef OPENFLOWACTIONCONVERTER_H
+#define OPENFLOWACTIONCONVERTER_H
+
+#include <eos/types/directflow.h>
+
+#include "OpenFlowRelease.h"
+
+class OpenFlowActionConverter
+{
+    public:
+        OpenFlowActionConverter(const eos::flow_action_t& action);
+        OpenFlowActionConverter(const std::istream is);
+
+        size_t ToBuffer(OpenFlowRelease release, std::ostream& os);
+        bool ToFlowAction(OpenFlowRelease release, eos::flow_action_t& action);
+};
+
+#endif // OPENFLOWACTIONCONVERTER_H
---
+++ master/include/lib/eos/OpenFlowRelease.h
@@ -0,0 +1,12 @@
+#ifndef OPENFLOWRELEASE_H_INCLUDED
+#define OPENFLOWRELEASE_H_INCLUDED
+
+enum class OpenFlowRelease : uint8_t
+{
+    openFlow_1_0,
+    openFlow_1_1,
+    openFlow_1_2,
+    openFlow_1_3
+};
+
+#endif // OPENFLOWRELEASE_H_INCLUDED
